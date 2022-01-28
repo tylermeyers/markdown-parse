e2683c99c843f9ba3a9130972a31cf9a04250a93
bimai25 <bimai@ucsd.edu>
2022-01-28 02:08:04 -0800

    fix bug 3
    fixed the issue if the link container "[]" was empty (which will not format as a link in markdown)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,16 +7,20 @@ import java.util.ArrayList;
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
         
+        //Split the contents of the markdown file into each individual line
         String[] contentsArray = markdown.split("\n");
+        //Loop through each line, check if it is of a link form
+        //if so, add the solely the link into the return ArrayList
         for(String s: contentsArray){
             if(isOfLinkForm(s))
                 toReturn.add(s.substring(s.indexOf("(")+1, s.length()-1));
         }
         /* Original Code
         -----------------------
+        // find the next [, then find the ], then find the (, then take up to
+        // the next )
+
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
@@ -30,7 +34,18 @@ public class MarkdownParse {
         return toReturn;
     }
     public static boolean isOfLinkForm(String s){
-        return s.contains("[") && s.contains("]") && s.contains("(") && s.contains(")") && !s.startsWith("!");
+        int firstBracket = s.indexOf("[");
+        int secondBracket = s.indexOf("]");
+        boolean containerGood = false;
+        String linkContainer = "";
+        //this obtains the whole container like [Link]
+        if (firstBracket >= 0 && secondBracket >= 0)
+            linkContainer = s.substring(firstBracket,secondBracket+1);
+        //link container would be valid if it's length is greater than 2 (it's not just "[]")
+        if(linkContainer.length() > 2)
+            containerGood = true;
+
+        return containerGood && s.contains("(") && s.contains(")") && !s.startsWith("!");
     }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
