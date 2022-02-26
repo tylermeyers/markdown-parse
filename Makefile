6f549897cd81e982e3c041089eed723de2d13abf
tylermeyers <88351548+tylermeyers@users.noreply.github.com>
2022-02-25 16:01:38 -0800

    f

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
+# test: MarkdownParse.class MarkdownParseTest.class
+# 	/software/CSE/oracle-java-se-14/jdk-14.0.2/bin/java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
+# MarkdownParse.class: MarkdownParse.java
+# 	/software/CSE/oracle-java-se-14/jdk-14.0.2/bin/javac MarkdownParse.java
+
+# MarkdownParseTest.class: MarkdownParseTest.java MarkdownParse.class
+# 	/software/CSE/oracle-java-se-14/jdk-14.0.2/bin/javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
+test: MarkdownParse.class MarkdownParseTest.class
+	java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+
 MarkdownParse.class: MarkdownParse.java
 	javac MarkdownParse.java
 
-MarkdownParseTest.class: MarkdownParseTest.java
-	javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java MarkdownParse.java
-
-test: MarkdownParse.java MarkdownParse.class MarkdownParseTest.class
-	java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+MarkdownParseTest.class: MarkdownParseTest.java MarkdownParse.class
+	javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java
--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,22 +7,8 @@ import java.util.ArrayList;
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-
-        //Split the contents of the markdown file into each individual line
-        String[] contentsArray = markdown.split("\n");
-        //Loop through each line, check if it is of a link form
-        //if so, add the solely the link into the return ArrayList
-        for(String s: contentsArray){
-            if(isOfLinkForm(s))
-                toReturn.add(s.substring(s.indexOf("(")+1, s.lastIndexOf(")")));
-                //using lastIndexOf() to fix a whitespace issue (I think) on
-                //windows
-        }
-        /* Original Code
-        -----------------------
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-
         int currentIndex = 0;
         int openParen = 0;
         int nextOpenBracket = 0;
@@ -30,57 +16,26 @@ public class MarkdownParse {
         int closeParen = 0;
         while(currentIndex < markdown.length()) {
             nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if (currentIndex > 0 && markdown.indexOf("!", currentIndex) == nextOpenBracket - 1) {
+            if (nextOpenBracket > 0 && markdown.indexOf("!", currentIndex) == nextOpenBracket - 1) {
                 currentIndex = nextOpenBracket + 1;
-                continue;
-            }
-            if (nextOpenBracket >= 0) {
-                openParen = markdown.indexOf("(", currentIndex);
-                nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-                if (openParen > 0 && nextCloseBracket == openParen - 1) {
-                    closeParen = markdown.indexOf(")", openParen);
-                    if (closeParen >= 0) {
-                        if (markdown.substring(openParen + 1, closeParen).length() > 0) {
+            } else {
+                if (nextOpenBracket >= 0) {
+                    openParen = markdown.indexOf("(", nextOpenBracket);
+                    nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                    if (openParen > 0 && nextCloseBracket == openParen - 1) {
+                        closeParen = markdown.indexOf(")", openParen);
+                        if (closeParen >= 0 && markdown.substring(openParen + 1, closeParen).length() > 0) {
                             toReturn.add(markdown.substring(openParen + 1, closeParen));
                         }
                     }
+                } else {
+                    break;
                 }
-            } else {
-                break;
             }
 
-            currentIndex = openParen + 1;
+            currentIndex = nextOpenBracket + 1;
         }
-        //*/
-        //System.out.println(toReturn.size());
         return toReturn;
-    }
-    public static boolean isOfLinkForm(String s){
-        int firstBracket = s.indexOf("[");
-        int pivotalSeq = s.indexOf("](");
-        boolean containerGood = false;
-        String linkContainer = "";
-        //this obtains the whole container like [Link]
-        if (firstBracket >=0 && pivotalSeq > firstBracket)
-            linkContainer = s.substring(firstBracket,pivotalSeq+1);
-        //Check linkContainer against container parameters
-        containerGood = checkLinkContainer(linkContainer);
-
-        return containerGood && s.contains("(") && s.contains(")") && !s.startsWith("!");
-    }
-
-    public static boolean checkLinkContainer(String container){
-        int numOpenBrackets = 0;
-        int numClosedBrackets = 0;
-        String toParse = container;
-        for (int i = 0; i < toParse.length(); i++){
-            if(toParse.substring(i,i+1).equals("["))
-                numOpenBrackets++;
-            else if(toParse.substring(i,i+1).equals("]"))
-                numClosedBrackets++;
-        }
-        return container.length() > 2 && numOpenBrackets >= 1 &&
-            numClosedBrackets >= 1 && numOpenBrackets >= numClosedBrackets;
     }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
